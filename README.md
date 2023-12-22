86b21117c5e393950191ae6ef5540522239ef7db
jonathan.ng <j6n.work@gmail.com>
2023-12-22 02:01:40 -0500

    config changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Y'hello!
-I am a site reliability engineer with interest in software development, homelabs and game development.
+I am a site reliability engineer with an interest in homelab and game development.
 My hobbies involve keyboards, cooking, fishing, video games, and music.
--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
       <div className='grid grid-cols-1 lg:grid-cols-12'>
         <div className='col-span-6'>
           <h1 className='col-span-6 text-4xl'>
-            <code className='font-extrabold'>Hello World</code>, I'm Jon (j6n)
+            <code className='font-extrabold'>Hello World</code>, I'm Jon [j6n]
           </h1>
           <br/>
           <p className='text-lg'>
-            I am a site reliability engineer with interest in software development, homelabs and game development.
+            I am a site reliability engineer with an interest in homelab and game development.
             My hobbies involve keyboards, cooking, fishing, video games, and music.
             Reach me at:&nbsp;
             <a href = 'mailto:me@j6n.ca'>me@j6n.ca</a>. 
-            You can <button>download my cv here</button> or, alternatively, view it via this request: <br/>
+            You can <a href = '/resume.pdf' target='_blank'>download my cv here</a> or, alternatively, view it via this request: <br/>
             <code className="font-mono font-bold">
               curl https://blablahblah
             </code>
