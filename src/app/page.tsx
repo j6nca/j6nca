9ff294810f8b95e19ca0ace6cb7182787d4bd26b
jonathan.ng <j6n.work@gmail.com>
2023-12-22 01:38:02 -0500

    fixing readmes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 container mx-auto">
       <Hero/>
       <Contact/>
-      
-      
     </main>
   )
 }
