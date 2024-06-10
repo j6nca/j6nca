32d809e006223dffceb9cd332442980d30d63791
jonathan ng <j6n.work@gmail.com>
2024-06-10 10:02:14 -0400

    update hero

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,13 +9,14 @@ const Hero = () => {
           <h1 className='col-span-6 text-xl md:text-4xl'>
             <code className='font-extrabold'>Hello World</code>, I&apos;m Jon <span className='highlight'>[j6n]</span>
           </h1>
-          <br/>
+          <br />
           <p className='text-sm md:text-2xl'>
-            I am a site reliability engineer based in Toronto, Canada. I have an interest in homelab and game development.
+            I am an observability and reliability engineer and a maker-of-things based in Toronto, Canada. I have an interest in homelab and game development.
             My hobbies involve custom keyboards, cooking, fishing, and video games.
+            <a className='highlight' href='https://github.com/users/j6nca/projects/2/views/1'>View what I'm currently working on</a>.
             Reach me at:&nbsp;
-            <a className='highlight' href = 'mailto:me@j6n.ca'>me@j6n.ca</a>. 
-            You can download my cv <a className='highlight' href = './resume' target='_blank'>here</a> or, alternatively, view it via this request: <br/>
+            <a className='highlight' href='mailto:me@j6n.ca'>me@j6n.ca</a>.
+            You can download my cv <a className='highlight' href='./resume' target='_blank'>here</a> or, alternatively, view it via this request: <br />
             <code className="font-mono font-bold">
               curl https://j6n.ca/resume.json
             </code>
@@ -32,7 +33,7 @@ const Hero = () => {
               height={300}
               className='rounded-full'
             />
-            
+
           </div>
         </div>
       </div>
