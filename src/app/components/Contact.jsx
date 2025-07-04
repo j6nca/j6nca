abeeb3f48241c2ad72201864beb418b5006498c5
jonathan ng <me@j6n.ca>
2025-07-03 23:03:59 -0400

    update socials

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -8,9 +8,11 @@ const Contact = () => {
       <ul className='flex flex-col sm:flex-row'>
         <div className='grid grid-cols-4 gap-2'>
           <ContactItem link='https://github.com/j6nca' name='github' />
-          <ContactItem link='https://www.linkedin.com/in/j6n' name='linkedin' />
           <ContactItem link='https://notes.j6n.ca' name='blog' />
+          <ContactItem link='https://monkeytype.com/profile/j6n' name='monkeytype' />
           <ContactItem link='mailto:me@j6n.ca' name='email' />
+          <ContactItem link='https://www.linkedin.com/in/j6n' name='linkedin' />
+          <ContactItem link='./resume' name='resume' />
         </div>
       </ul>
     </section>
