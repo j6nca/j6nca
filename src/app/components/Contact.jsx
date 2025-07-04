f7396709e80e5af9fb827f464169b2bedba17616
jonathan ng <me@j6n.ca>
2025-07-04 19:58:11 -0400

    add kube-cats

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -9,6 +9,7 @@ const Contact = () => {
         <div className='grid grid-cols-6 gap-2'>
           <ContactItem link='https://github.com/j6nca' name='github' />
           <ContactItem link='https://notes.j6n.ca' name='blog' />
+          <ContactItem link='https://meow.j6n.dev' name='kube-cats' />
           <ContactItem link='https://monkeytype.com/profile/j6n' name='monkeytype' />
           <ContactItem link='mailto:me@j6n.ca' name='email' />
           <ContactItem link='https://www.linkedin.com/in/j6n' name='linkedin' />
